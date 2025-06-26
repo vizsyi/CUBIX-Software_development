a0345922075dd84102558655a215fe8b2435d7e8
Vizsy István <steve.visy@gmail.com>
2025-06-26 22:23:27 +0200

    #3: Fibonacci program after using Quality tools

--- Web/Homeworks/HW-Week2/Fibonacci.js
+++ Web/Homeworks/HW-Week2/Fibonacci.js
@@ -1,32 +1,29 @@
 //const fibonacciBtn = document.getElementById("fibonaccibtn");
 //fibonacciBtn.onclick(() => printFibonacci());
 
-
 function generateFibonacci(count) {
     if (count < 1) throw new RangeError("Érvénytelen Fibonacci paraméter");
 
-    let fib = [0, 1], noUse;
+    const fib = [0, 1];
 
-    if (count = 9) console.log("Ez a kedvenc számom!");
-
-    if (count > 2){
-    for (i = 3; i <= count; i++){
-        fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
-    }
-    }
-    else {
-    fib.length = count;
+    if (count > 2) {
+        for (let i = 3; i <= count; i++) {
+            fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+        }
+    } else {
+        fib.length = count;
     }
 
     return fib;
 }
 
-function printFibonacci(){
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function printFibonacci() {
     const count = parseInt(document.getElementById("count").value);
     const output = document.getElementById("output");
 
     if (isNaN(count) || count < 1) {
-        output.textContent = 'Kérlek, adj meg egy pozitív egész számot.';
+        output.textContent = "Kérlek, adj meg egy pozitív egész számot.";
         return;
     }
 
